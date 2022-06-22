e6993f841cf3e452d6cdd38dc47c58a215085656
Robby <robbygardner@gmail.com>
2022-06-21 17:01:52 -0700

    Created event listeners, added validation function, and added regex tests

--- regex-ninja.js
+++ regex-ninja.js
@@ -1,3 +1,25 @@
-let reg = /[a-z]/gi;
+const inputs = document.querySelectorAll("input");
 
-let reg2 = new RegExp(/[a-z]/, "i");
+const patterns = {
+  username: /^[a-z\d]{6,14}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i,
+  password: /^[\w@-]{8,20}$/,
+  telephone: /^\d{10}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+};
+
+//validation function
+function validate(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    //console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
